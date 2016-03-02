c1191beeb23ce15e4555f864bdea6618045f3933
Alex Karahanidi <alexka1991@mail.ru>
2016-03-02 13:51:27 +0600

    add category form

--- forms.py
+++ forms.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.forms import ModelForm
-from .models import Product, Image
+from .models import Category, Product, Image
+
+
+class CategoryForm(ModelForm):
+	class Meta:
+		model = Category
+		fields = ['name', 'slug', 'parent', 'order']
 
 
 class ProductForm(ModelForm):
